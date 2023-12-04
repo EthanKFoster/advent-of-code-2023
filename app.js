0bc0b3b3a5e900b6e8f688e6ef6e0154cc439f17
EthanKFoster <ethankfoster@gmail.com>
2023-12-04 15:34:29 -0800

    update app.js with more puzzle index values

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 import solve from "./tools/solve.js"
 
 let puzzles = [
-  'day-1-part-1', // 0
-  'day-1-part-2', // 1
-  'day-2-part-1', // 2
+  "day-1-part-1", // 0
+  "day-1-part-2", // 1
+  "day-2-part-1", // 2
+  "day-2-part-2", // 3
+  "day-3-part-1", // 4
+  "day-3-part-2", // 5
+  "day-4-part-1", // 6
+  "day-4-part-2", // 7
 ]
 
-let puzzleToPrint = puzzles[2]
+let puzzleToPrint = puzzles[6]
 
 solve(puzzleToPrint)
