c06361520ad0b29dcf1b5db7a1e8401d7d32ecb0
EthanKFoster <ethankfoster@gmail.com>
2023-12-06 12:55:48 -0800

    complete days array

--- app.js
+++ app.js
@@ -9,6 +9,48 @@ let puzzles = [
   "day-3-part-2", // 5
   "day-4-part-1", // 6
   "day-4-part-2", // 7
+  "day-5-part-1", // 8
+  "day-5-part-2", // 9
+  "day-6-part-1", // 10
+  "day-6-part-2", // 11
+  "day-7-part-1", // 12
+  "day-7-part-2", // 13
+  "day-8-part-1", // 14
+  "day-8-part-2", // 15
+  "day-9-part-1", // 16
+  "day-9-part-2", // 17
+  "day-10-part-1", // 18
+  "day-10-part-2", // 19
+  "day-11-part-1", // 20
+  "day-11-part-2", // 21
+  "day-12-part-1", // 22
+  "day-12-part-2", // 23
+  "day-13-part-1", // 24
+  "day-13-part-2", // 25
+  "day-14-part-1", // 26
+  "day-14-part-2", // 27
+  "day-15-part-1", // 28
+  "day-15-part-2", // 29
+  "day-16-part-1", // 30
+  "day-16-part-2", // 31
+  "day-17-part-1", // 32
+  "day-17-part-2", // 33
+  "day-18-part-1", // 34
+  "day-18-part-2", // 35
+  "day-19-part-1", // 36
+  "day-19-part-2", // 37
+  "day-20-part-1", // 38
+  "day-20-part-2", // 39
+  "day-21-part-1", // 40
+  "day-21-part-2", // 41
+  "day-22-part-1", // 42
+  "day-22-part-2", // 43
+  "day-23-part-1", // 44
+  "day-23-part-2", // 45
+  "day-24-part-1", // 46
+  "day-24-part-2", // 47
+  "day-25-part-1", // 48
+  "day-25-part-2", // 49
 ]
 
 let puzzleToPrint = puzzles[6]
